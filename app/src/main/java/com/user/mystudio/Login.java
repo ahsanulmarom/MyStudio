00031796bf14b2a2c7903f3f1dd9298810b1f57c
ahsanulmarom <ahsanulmarom@gmail.com>
2017-09-21 10:14:16 +0700

    signup login belum mirip gui

---
+++ app/src/main/java/com/user/mystudio/Login.java
@@ -0,0 +1,189 @@
+package com.user.mystudio;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button submit, daftar, reset;
+    private FirebaseAuth fAuth;
+    private FirebaseAuth.AuthStateListener fStateListener;
+    private static final String TAG = Login.class.getSimpleName();
+    private Context context = this;
+    CheckNetwork cn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        cn = new CheckNetwork(this);
+        if (!cn.isConnected()) {
+            Toast.makeText(this, "You are not connected internet. Pease check your connection!", Toast.LENGTH_LONG).show();
+        }
+
+        fAuth = FirebaseAuth.getInstance();
+        fStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        // User sedang login
+                        Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    } else {
+                        // User sedang logout
+                        Log.d(TAG, "onAuthStateChanged:signed_out");
+                    }
+                }
+            };
+
+            email = (EditText) findViewById(R.id.login_email);
+            password = (EditText) findViewById(R.id.login_password);
+            submit = (Button) findViewById(R.id.login_btnLogin);
+            daftar = (Button) findViewById(R.id.login_btnSignUp);
+            reset = (Button) findViewById(R.id.login_reset);
+
+            submit.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (email.getText().toString().equalsIgnoreCase("")) {
+                        email.setError("This Field is Required");
+                    } else if (password.getText().toString().equalsIgnoreCase("")) {
+                        password.setError("This Field is Required");
+                    } else {
+                        login(email.getText().toString(), password.getText().toString());
+                    }
+
+                }
+            });
+
+            daftar.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(Login.this, SignUp.class));
+                    finish();
+                }
+            });
+
+            reset.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    LinearLayout layoutinput = new LinearLayout(context);   //layout
+                    layoutinput.setOrientation(LinearLayout.VERTICAL);
+                    layoutinput.setPadding(50, 50, 50, 50);
+
+                    final EditText mail = new EditText(context);
+                    mail.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                    mail.setHint("Enter your verified Email!");
+                    layoutinput.addView(mail);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Reset Password");
+                    builder.setMessage("Please enter your verified and active email address below." +
+                            "We will send you an email with instruction for resetting your password.");
+                    builder.setView(layoutinput);
+                    //negative button
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    //posstive button
+                    builder.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (!cn.isConnected()) {
+                                Toast.makeText(Login.this, "You are offline. Pease check your connection!",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                sendEmailResetPassword(mail.getText().toString().trim());
+                            }
+                        }
+                    });
+                    builder.show();
+                }
+            });
+    }
+
+    private void login(final String email, String password){
+        fAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            if (!cn.isConnected()) {
+                                Toast.makeText(Login.this, "You are offline. Pease check your connection!",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Login.this, "Sorry, Your Email or Password is Incorrect. Please try again!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (task.isSuccessful()) {
+                            checkIfEmailVerified();
+                        }
+                    }
+                });
+    }
+
+    private void checkIfEmailVerified() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user.isEmailVerified()) {
+            // user is verified, so you can finish this activity or send user to activity which you want.
+            startActivity(new Intent(Login.this, Home.class));
+        }
+        else {
+            Toast.makeText(Login.this, "Sorry, Please Check Email and Verify Your Account!",
+                    Toast.LENGTH_SHORT).show();
+            FirebaseAuth.getInstance().signOut();
+        }
+    }
+
+    private void sendEmailResetPassword(String emailAd) {
+        fAuth = FirebaseAuth.getInstance();
+        fAuth.sendPasswordResetEmail(emailAd).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "The instruction has been sent to your email. Please check your email!",
+                            Toast.LENGTH_SHORT).show();
+                } else if(!task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Request failed. Pelase, try again!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fAuth.addAuthStateListener(fStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (fStateListener != null) {
+            fAuth.removeAuthStateListener(fStateListener);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/user/mystudio/SignUp.java
@@ -0,0 +1,186 @@
+package com.user.mystudio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUp extends AppCompatActivity {
+
+    private EditText username, email, password, repassword;
+    String uname, mail;
+    private Button submit;
+    private FirebaseAuth fAuth;
+    private FirebaseAuth.AuthStateListener fStateListener;
+    private static final String TAG = SignUp.class.getSimpleName();
+    CheckNetwork cn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        cn = new CheckNetwork(this);
+        if (!cn.isConnected()) {
+            Toast.makeText(this, "You are not connected internet. Pease check your connection!", Toast.LENGTH_SHORT).show();
+        }
+            fAuth = FirebaseAuth.getInstance();
+            fStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        // User sedang login
+                        if (!(user.isEmailVerified())) {
+                            sendVerificationEmail();
+                        }
+                        Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    } else {
+                        // User sedang logout
+                        Log.d(TAG, "onAuthStateChanged:signed_out");
+                    }
+                }
+            };
+
+            username = (EditText) findViewById(R.id.signup_username);
+            uname = username.getText().toString().trim();
+            email = (EditText) findViewById(R.id.signup_email);
+            mail = email.getText().toString().trim();
+            password = (EditText) findViewById(R.id.signup_password);
+            repassword = (EditText) findViewById(R.id.signup_repassword);
+            submit = (Button) findViewById(R.id.signup_submit);
+
+            submit.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (username.getText().toString().equalsIgnoreCase("")) {
+                        username.setError("This Field is Required");
+                    } else if (username.getText().toString().length() < 5) {
+                        username.setError("Username must be at least 5 characters");
+                    } else if (email.getText().toString().equalsIgnoreCase("")) {
+                        email.setError("This Field is Required");
+                    } else if (password.getText().toString().equalsIgnoreCase("")) {
+                        password.setError("This Field is Required");
+                    } else if (password.getText().toString().length() < 5) {
+                        password.setError("Password must be at least 5 characters");
+                    } else if (!(repassword.getText().toString().equals(password.getText().toString()))) {
+                        repassword.setError("Please check your password!");
+                    } else {
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference usernama = database.getReference("userData");
+                        usernama.orderByChild("username").equalTo(username.getText().toString().trim()).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                Log.e(TAG, "onDataChange: asasa " + dataSnapshot.getRef());
+                                if (dataSnapshot.getRef() == null) {
+                                    signUp(email.getText().toString(), password.getText().toString());
+                                } else {
+                                    Toast.makeText(SignUp.this, "Username already exist. Please try another username.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+
+                }
+            });
+    }
+
+    private void signUp(final String mail, String password) {
+        fAuth.createUserWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            if (!cn.isConnected()) {
+                                Toast.makeText(SignUp.this, "You are offline. Pease check your connection!",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(SignUp.this, "Failed to sign up. Email has been registered.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } else if (task.isSuccessful()) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference userData = database.getReference("userData").child(user.getUid());
+                            Map data = new HashMap();
+                            data.put("email", email.getText().toString().trim());
+                            data.put("username", username.getText().toString().trim());
+                            data.put("displayName", 0);
+                            data.put("role", 0);
+                            data.put("bio", "Hello! I'm a new member here.");
+                            data.put("address", 0);
+                            userData.setValue(data);
+                            Toast.makeText(SignUp.this, "Sign up Successfully. Please check email to verify account! ",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // email sent
+                            // after email is sent just logout the user and finish this activity
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(SignUp.this, Login.class));
+                            finish();
+                        }
+                        else {
+                            // email not sent, so display message and restart the activity or do whatever you wish to do
+                            //restart this activity
+                            overridePendingTransition(0, 0);
+                            finish();
+                            overridePendingTransition(0, 0);
+                            startActivity(getIntent());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fAuth.addAuthStateListener(fStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (fStateListener != null) {
+            fAuth.removeAuthStateListener(fStateListener);
+        }
+    }
+}
