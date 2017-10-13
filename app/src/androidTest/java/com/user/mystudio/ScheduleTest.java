8b7b863b80b274e3a0508893da3ee0f45771c65f
ahsanulmarom <ahsanulmarom@gmail.com>
2017-10-13 14:52:48 +0700

    Merge branch 'master' of https://github.com/ahsanulmarom/MyStudio
    # Conflicts:
    #	app/src/androidTest/java/com/user/mystudio/ScheduleTest.java

--- app/src/androidTest/java/com/user/mystudio/ScheduleTest.java
+++ app/src/androidTest/java/com/user/mystudio/ScheduleTest.java
@@ -1,31 +1,19 @@
 package com.user.mystudio;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-import java.lang.reflect.Method;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
-import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
