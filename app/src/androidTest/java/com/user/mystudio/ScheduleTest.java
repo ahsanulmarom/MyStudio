115737cd40c0c6c91c795d255f0fb63740c406b8
Edwin Dwi Ahmad <edwindwiahmad96@gmail.com>
2017-10-13 14:36:23 +0700

    testing no schedule

--- app/src/androidTest/java/com/user/mystudio/ScheduleTest.java
+++ app/src/androidTest/java/com/user/mystudio/ScheduleTest.java
@@ -1,12 +1,34 @@
 package com.user.mystudio;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+
+import java.lang.reflect.Method;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by Edwin Dwi Ahmad on 12-Oct-17.
@@ -16,7 +38,7 @@ import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.DEFAULT)
 public class ScheduleTest {
     @Rule
-    public ActivityTestRule<Login> scheduleActivityActivityTestRule = new ActivityTestRule<>(Schedule.class, true, false);
+    public ActivityTestRule<Login> loginActivityTestRule = new ActivityTestRule<>(Login.class, true, false);
 
     private void pauseTestFor(long milliseconds) {
         try {
@@ -26,5 +48,24 @@ public class ScheduleTest {
         }
     }
 
-
+    @Test
+    public void nullSchedule() {
+        loginActivityTestRule.launchActivity(null);
+        onView(withId(R.id.login_email)).perform(typeText("ahsanulmarom.amamam@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.login_password)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.login_btnLogin)).perform(click());
+        pauseTestFor(7000);
+        onView(withId(R.id.menu_booking)).perform(click());
+        pauseTestFor(500);
+        onView(withId(R.id.booking_schedule)).perform(click());
+        pauseTestFor(5000);
+        onView(withText("No Schedule")).check(matches(isDisplayed()));
+        pauseTestFor(1000);
+        onView(withText("Booking Photo")).perform(click());
+        pauseTestFor(1000);
+        onView(withId(R.id.booking_schedule)).perform(click());
+        pauseTestFor(1000);
+        onView(withText("Cancel")).perform(click());
+        pauseTestFor(1000);
+    }
 }
